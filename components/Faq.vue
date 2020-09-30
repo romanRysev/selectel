e7e4c21f48acdfc932392897140c7cf883f0b582
Roman Rysev <roman.rysev@gmail.com>
2020-10-01 02:24:44 +0300

    интерактивность в faq

--- components/Faq.vue
+++ components/Faq.vue
@@ -5,12 +5,12 @@
     </h2>
     <div class="faq__item-container">
       <div v-for="item in data.items" :key="item.id" class="faq__item">
-        <div class="faq__item-header-container">
+        <div class="faq__item-header-container" @click="boxToggler">
           <div class="faq__item-question h3-desktop" v-html="item.question" />
-          <img class="faq__item-open-icon" src="plus.svg" alt="Развернуть">
-          <img class="faq__item-close-icon" src="minus.svg" alt="Свернуть">
+          <img class="faq__item-open-icon faq__item-icon" src="plus.svg" alt="Развернуть">
+          <img class="faq__item-close-icon faq__item-icon hidden" src="minus.svg" alt="Свернуть">
         </div>
-        <div class="faq__item-answer p2-desktop" v-html="item.answer" />
+        <div class="faq__item-answer p2-desktop hidden" v-html="item.answer" />
       </div>
     </div>
   </section>
@@ -22,11 +22,37 @@ export default {
     data () {
       return this.$store.getters['data/getFaqData']
     }
+  },
+  methods: {
+    boxToggler () {
+      if (event.target.classList.contains('faq__item-header-container')) {
+        event.target.parentElement.lastElementChild.classList.toggle('hidden')
+        event.target.firstElementChild.nextElementSibling.classList.toggle('hidden')
+        event.target.firstElementChild.nextElementSibling.nextElementSibling.classList.toggle('hidden')
+      }
+
+      if (event.target.classList.contains('faq__item-question')) {
+        event.target.parentElement.parentElement.lastElementChild.classList.toggle('hidden')
+        event.target.nextElementSibling.classList.toggle('hidden')
+        event.target.nextElementSibling.nextElementSibling.classList.toggle('hidden')
+      }
+
+      if (event.target.classList.contains('faq__item-icon')) {
+        event.target.parentElement.parentElement.lastElementChild.classList.toggle('hidden')
+        event.target.parentElement.firstElementChild.nextElementSibling.classList.toggle('hidden')
+        event.target.parentElement.firstElementChild.nextElementSibling.nextElementSibling.classList.toggle('hidden')
+      }
+    }
   }
 }
 </script>
 
 <style>
+
+.hidden {
+  display: none;
+}
+
 .faq {
   max-width: 740px;
   padding-left: 46px;
@@ -41,7 +67,8 @@ export default {
 }
 
 .faq__item {
-  padding: 23px 0 28px;
+  padding-top: 23px;
+  padding-bottom: 4px;
   border-top: 1px solid #d9d9d6;
 }
 
@@ -50,31 +77,29 @@ export default {
   align-items: center;
   justify-content: space-between;
   margin-bottom: 24px;
-}
-
-.faq__item-open-icon {
-  display: none;
+  cursor: pointer;
 }
 
 .faq__item-answer {
   margin-left: 250px;
+  margin-bottom: 20px;
 }
 
 @media screen and (max-width: 770px) {
   .faq {
     padding-top: 64px;
     padding-bottom: 64px;
+    padding: 64px 0 64px 140px;
   }
 
   .faq__item-container {
     display: flex;
     flex-direction: column;
-    align-items: center;
   }
 
   .faq__item {
     max-width: 355px;
-    margin-left: 75px;
+    margin-left: 130px;
   }
   .faq__item-answer {
     margin-left: 60px;
