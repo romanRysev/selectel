ba3791c65e9249cd4fae364ef9affbd268ce17b7
Roman Rysev <roman.rysev@gmail.com>
2020-10-01 04:46:43 +0300

    HTML lang

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,26 +6,23 @@ export default {
   },
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
+    htmlAttrs: {
+      lang: 'ru'
+    },
     title: 'selectel',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' }
     ],
-    link: [
-      { rel: 'icon', type: 'image/svg+xml', href: 'favicon.svg' }
-    ]
+    link: [{ rel: 'icon', type: 'image/svg+xml', href: 'favicon.svg' }]
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-    '~assets/css/main.css',
-    '~assets/fonts/fonts.css'
-  ],
+  css: ['~assets/css/main.css', '~assets/fonts/fonts.css'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
@@ -47,6 +44,5 @@ export default {
   axios: {},
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-  }
+  build: {}
 }
